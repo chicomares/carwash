5ea702045bc33878ad0e346d8a32315479a3cbbf
chicomares <mareschico1@gmail.com>
2017-06-10 22:37:24 +0100

    Create script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function showMore(){
     //shows the #more
     document.getElementById('more').style.display = "block";
 }
+
+$(function(){ /* to make sure the script runs after page load */
+
+    $('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+
+        event.preventDefault(); /* prevent the a from changing the url */
+        $(this).parents('.bumbum').find('.more_text').show(); /* show the .more_text span */
+
+    });
+
+});
